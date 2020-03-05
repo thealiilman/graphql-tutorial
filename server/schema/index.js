246916cdecf171e2d17c1006c227739bbf07da9b
thealiilman <aliilman532@gmail.com>
2020-03-05 10:51:06 +0800

    Finished GraphQL Tutorial #12 - Author Type.

--- server/schema/index.js
+++ server/schema/index.js
@@ -3,12 +3,19 @@ const graphql = require('graphql')
 const {
   GraphQLObjectType,
   GraphQLString,
+  GraphQLInt,
   GraphQLSchema,
   GraphQLID,
 } = graphql;
 
+const authors = [
+  { name: 'Julian Barnes', age: 74, id: '1' },
+  { name: 'Jeffrey Archer', age: 79, id: '2' },
+  { name: 'Edward Rutherfurd', age: 72, id: '3' }
+]
+
 const books = [
-  { name: 'And Then There Were None', genre: 'Mystery', id: '1' },
+  { name: 'The Sense of an Ending', genre: 'Drama', id: '1' },
   { name: 'Nothing Ventured', genre: 'Drama', id: '2' },
   { name: 'London', genre: 'Historical Fiction', id: '3' },
 ]
@@ -19,6 +26,15 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+  }),
+})
+
+const AuthorType = new GraphQLObjectType({
+  name: 'Author',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 })
 
@@ -34,6 +50,15 @@ const RootQuery = new GraphQLObjectType({
         // code to get data from db / other sources
         return books.find(book => book.id === id)
       },
+    },
+    author: {
+      type: AuthorType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve: (parent, { id }) => (
+        authors.find(author => author.id === id)
+      ),
     },
   },
 })
