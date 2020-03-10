4d6f90ee13e12d4ee2498e335d34a812e9ae1e74
thealiilman <aliilman532@gmail.com>
2020-03-10 14:50:42 +0800

    Finished GraphQL Tutorial #23-25.

---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function BookList() {
+  return (
+    <div>
+      <ul id="book-list">
+        <li>And Then There Were None</li>
+      </ul>
+    </div>
+  );
+}
+
+export default BookList;
